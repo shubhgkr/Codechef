65215f1631071b0ab214426abf7734217abc2f3f
shubhgkr <shubhgkr@gmail.com>
2019-07-31 21:29:14 +0530

    START01 problem.

--- Beginner/README.md
+++ Beginner/README.md
@@ -1,3 +1,4 @@
 # Beginner Questions of Codechef
 
 -   [DECINC](https://www.codechef.com/problems/DECINC)
+-   [START01](https://www.codechef.com/problems/START01)
---
+++ Beginner/START01.cpp
@@ -0,0 +1,15 @@
+/*
+ Created by shubhgkr on 31/07/19.
+ https://www.codechef.com/problems/START01
+*/
+
+#include <iostream>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int n;
+	std::cin >> n;
+	std::cout << n;
+	return 0;
+}
