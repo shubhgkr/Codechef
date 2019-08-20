2792430b20ccedc8fb89e0c79af078cd3a641764
shubhgkr <shubhgkr@gmail.com>
2019-08-20 23:39:00 +0530

    Basic BST search function.

--- Algorithms/BST.cpp
+++ Algorithms/BST.cpp
@@ -27,12 +27,21 @@ Node *insertNewNode(Node *node, int data) {
 	return node;
 }
 
+bool search(Node *node, int data) {
+	if (node == nullptr)
+		return false;
+	if (node->data == data)
+		return true;
+	if (data < node->data)
+		return search(node->left, data);
+	return search(node->right, data);
+}
+
 int main() {
 	int arr[] = {15, 20, 25, 10, 8, 12, 18};
 	Node *root = nullptr;
 	for (int v:arr)
 		root = insertNewNode(root, v);
-	if (root)
-		std::cout << root->data;
+	std::cout << search(root, 55);
 	return 0;
 }
