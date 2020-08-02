d41ea9ee009df8f0a009117d6622dd7ef8c45beb
shubhgkr <shubhgkr@gmail.com>
2020-08-02 14:33:01 +0530

    WALK problem.

---
+++ Easy/WALK.cpp
@@ -0,0 +1,27 @@
+/*
+ * @author Shubham Kumar Gupta (shubhgkr)
+ * github: http://www.github.com/shubhgkr
+ * Created on 8/2/20.
+ * Problem link: https://www.codechef.com/problems/WALK
+ */
+
+#include <iostream>
+#include <algorithm>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int t;
+	std::cin >> t;
+	while (t--) {
+		int n;
+		std::cin >> n;
+		int arr[n];
+		for (int i = 0; i < n; ++i) {
+			std::cin >> arr[i];
+			arr[i] += i;
+		}
+		std::cout << *std::max_element(arr, arr + n) << "\n";
+	}
+	return 0;
+}
